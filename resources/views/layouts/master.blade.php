df80341d9312a40122ab64c294e9da49e787a098
Michiel Meilink <michielmeilink@gmail.com>
2017-10-03 16:44:42 +0200

    Laravel | Setting up framework, header and import extern fonts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meilink Design</title>
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{ URL::asset("/resources/assets/css/layout.css") }}">
+</head>
+<body>
+    @include('layouts.nav')
+</body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="nav">
+            <a class="nav-link active" href="#">Diensten</a>
+            <a class="nav-link" href="#">Portfolio</a>
+            <a class="nav-link">
+                <img src="{{ URL::asset("/resources/meilinkdesign_logo.png") }}" width="100px">
+            </a>
+            <a class="nav-link" href="#">Over mij</a>
+            <a class="nav-link" href="#">Contact</a>
+        </nav>
+    </div>
+</div>
+
+
