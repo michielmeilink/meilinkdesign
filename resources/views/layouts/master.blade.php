9e1f1ae63b5e978a9e1a511517190bfe6f3cd30e
Michiel Meilink <michielmeilink@gmail.com>
2017-10-06 12:27:42 +0200

    Laravel | Home page and portfolio text (CSS) updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,14 +9,46 @@
 
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="{{ URL::asset("/resources/assets/css/layout.css") }}">
-</head>
-<body>
 
+    <!-- Ubuntu font -->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,700" rel="stylesheet">
+</head>
+
+<body>
     @include('layouts.nav')
 
-    <div class="master-bg">
-        <h1>Meilink Design</h1>
-    </div>
+    <section class="hero">
+        <h1 class="hero-title">Meilink Design</h1>
+    </section>
 
+    <section class="features">
+        <div class="features-group">
+            <div class="features-item">
+                <h1 class="text">Grafisch ontwerp</h1>
+            </div>
+            <div class="features-item">
+                <h1 class="text">Webdesign</h1>
+            </div>
+            <div class="features-item">
+                <h1 class="text">Social media</h1>
+            </div>
+        </div>
+    </section>
+
+    <section class="portfolio">
+        <div class="portfolio-group">
+            <div class="portfolio-group-left">
+                <p class="portfolio-text">
+                    <img src="{{ URL::asset("/resources/meilinkdesign_logo_text.svg") }}" width="150px">
+                </p>
+            </div>
+            <div class="portfolio-group-right">
+                <h2>Portfolio</h2>
+                <p class="portfolio-text">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.
+                </p>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,15 +1,13 @@
-<div class="masthead">
-    <div class="container">
-        <nav class="nav">
-            <a class="nav-link active" href="#">Diensten</a>
-            <a class="nav-link" href="#">Portfolio</a>
-            <a class="nav-link">
-                <img src="{{ URL::asset("/resources/meilinkdesign_logo.png") }}" width="100px">
-            </a>
-            <a class="nav-link" href="#">Over mij</a>
-            <a class="nav-link" href="#">Contact</a>
-        </nav>
-    </div>
+<div class="nav-container">
+    <nav class="nav">
+        <a class="nav-link active" href="#">Diensten</a>
+        <a class="nav-link" href="#">Portfolio</a>
+        <a class="nav-link">
+            <img src="{{ URL::asset("/resources/meilinkdesign_logo.svg") }}" width="100px">
+        </a>
+        <a class="nav-link" href="#">Over mij</a>
+        <a class="nav-link" href="#">Contact</a>
+    </nav>
 </div>
 
 
